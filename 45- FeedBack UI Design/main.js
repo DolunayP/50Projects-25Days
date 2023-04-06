e389112a422aa298b0a88c6c0d34abef85ee0698
Dolunay <pazarcidolunay@gmail.com>
2023-04-06 23:22:32 +0300

    Feedback UI Design Project Done

---
+++ 45- FeedBack UI Design/main.js
@@ -0,0 +1,39 @@
+const panel = document.querySelector('.panel-container')
+const ratingContainer = document.querySelector('.rating-container')
+const sendBtn = document.querySelector('.btn')
+const ratings = document.querySelectorAll('.rating')
+let feedback = 'Satisfied'
+
+
+ratingContainer.addEventListener('click', (e) => {
+    if (e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    } else if (
+        e.target.parentNode.classList.contains('rating') &&
+        e.target.previousSibling &&
+        e.target.previousElementSibling.nodeName === 'IMG'
+    ) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.innerHTML
+    }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support</p>
+    `
+})
+
+function removeActive() {
+    ratings.forEach(item => {
+        item.classList.remove('active')
+    })
+}
+
